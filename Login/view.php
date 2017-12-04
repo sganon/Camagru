57d4ba66365aba10055faa3e36446eade9358b7c
Simon GANON <simon@blueboard.io>
2017-12-04 17:42:21 +0100

    feat: use mvc architecture

---
+++ Login/view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Camagru - Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <a href="/index.php?signup=true">SignUp</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,43 +1,14 @@
 <?php
-	include "./config/setup.php";
-	var_dump($_COOKIE);
+	include 'config/setup.php';
+	session_start();
+	
+	if (empty($_SESSION["token"])) {
+		if ($_GET["signup"] == "true") {
+			require "SignUp/view.php";
+		} else {
+			require 'Login/view.php';
+		}
+	} else {
+		require 'Home/view.php';		
+	}
 ?>
-<html>
-	<head>
-		<title>Camagru</title>
-	</head>
-
-	<body>
-		<header id="header">
-			<h1>Camagru</h1>
-		</header>
-
-		<div id="main">
-			<?php if (isset($_COOKIE["user_id"])) :?>
-				User connected
-			<?php else:?>
-				<div id="suForm">
-					<label for="login">Login</label>
-					<input type="text" name="login" id="suLogin">
-					<span id="loginErr" class="err"></span><br>
-
-					<label for="email">Email</label>
-					<input type="text" name="email" id="suEmail">
-					<span id="emailErr" class="err"></span><br>
-
-					<label for="password">Password</label>
-					<input type="password" name="password" id="suPwd">
-					<span id="pwdErr" class="err"></span><br>
-
-					<label for="passwordConfirm">Confirm your password</label>					
-					<input type="password" name="passwordConfirm" id="suPwdConf">
-					<span id="pwdConfErr" class="err"></span><br>
-					
-					<input id="suSubmit" type="submit" value="Sign Up" onclick="createUser();">
-				</div>
-			<?php endif;?>
-		</div>
-
-		<script type="text/javascript" src="scripts/request.js"></script>				
-	</body>
-</html>
